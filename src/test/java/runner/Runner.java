7194a9ef6fed13e0939fc29089f3070dd975d38e
chetanbendre <chetan1109@gmail.com>
2019-01-30 09:45:33 +0530

    modifiedd runner class

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -7,7 +7,14 @@ import cucumber.api.junit.Cucumber;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions
+@CucumberOptions(
+		features = "src/test/java/testcases",
+		glue = "step_def",
+		tags = {},
+		dryRun= false,
+		monochrome = true
+		
+		)
 public class Runner {
 
 }
