a97f104560c41d58e14b30d3dce9961dbbce5c05
chetanbendre <chetan1109@gmail.com>
2019-02-28 20:35:27 +0530

    added apache poi dependency

--- src/test/java/generic/Base.java
+++ src/test/java/generic/Base.java
@@ -1,11 +1,37 @@
 package generic;
 
+
+import java.util.*;
+
 import junit.framework.Assert;
 
 public class Base {
+	
 	public void compareValue(Object actualvalue, Object expectedvalue) {
      Assert.assertEquals(expectedvalue, actualvalue);
      
 	}
 
+	
+	public void learningCollection() {
+		List<String> ll;
+		ll = new ArrayList<String>();
+		ll.add("sdfjb");
+		ll.add("5461");
+		ll.add("nknk");
+		
+		ll = new LinkedList();
+		
+		ll = new Vector();
+		Set ss;
+		ss = new HashSet();
+		ss = new LinkedHashSet();
+		ss = new TreeSet();
+		Map mm;
+		mm = new HashMap();
+		mm = new LinkedHashMap();
+		mm = new TreeMap();
+		
+		TreeMap hh = new TreeMap();		
+	}
 }
