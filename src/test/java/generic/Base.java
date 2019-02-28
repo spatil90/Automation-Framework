f099aa632f6720fc9ee0dc6981a02ebd2c2988ea
chetanbendre <chetan1109@gmail.com>
2019-02-28 20:37:38 +0530

    comment section

--- src/test/java/generic/Base.java
+++ src/test/java/generic/Base.java
@@ -13,25 +13,19 @@ public class Base {
 	}
 
 	
-	public void learningCollection() {
-		List<String> ll;
-		ll = new ArrayList<String>();
-		ll.add("sdfjb");
-		ll.add("5461");
-		ll.add("nknk");
-		
-		ll = new LinkedList();
-		
-		ll = new Vector();
-		Set ss;
-		ss = new HashSet();
-		ss = new LinkedHashSet();
-		ss = new TreeSet();
-		Map mm;
-		mm = new HashMap();
-		mm = new LinkedHashMap();
-		mm = new TreeMap();
-		
-		TreeMap hh = new TreeMap();		
-	}
+	/*
+	 * public void learningCollection() { List<String> ll; ll = new
+	 * ArrayList<String>(); ll.add("sdfjb"); ll.add("5461"); ll.add("nknk");
+	 * 
+	 * ll = new LinkedList();
+	 * 
+	 * ll = new Vector();
+	 * 
+	 * Set ss; ss = new HashSet(); ss = new LinkedHashSet(); ss = new TreeSet();
+	 * 
+	 * Map<Integer, String> mm; mm = new HashMap<Integer, String>(); mm = new
+	 * LinkedHashMap<Integer, String>(); mm = new TreeMap<Integer, String>();
+	 * 
+	 * TreeMap hh = new TreeMap(); }
+	 */
 }
